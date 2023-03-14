abde8211a99e99b965130abfdd2196a947f295f1
Alok Gokhe <alokg@alohatechnology.com>
2023-03-14 19:58:29 +0530

    login WIP

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,14 @@ import JobResultsView from "@/views/JobResultsView.vue";
 import ProfilePage from "@/components/Resources/ProfilePage.vue";
 import JobView from "@/views/JobViews.vue";
 import TeamsView from "@/views/TeamsView.vue";
-
+import LoginPage from "@/views/LoginPage.vue";
 const routes = [
   { path: "/", name: "Home", component: HomeView },
   { path: "/jobs/result", name: "JobResults", component: JobResultsView },
   { path: "/profilepage/:id", name: "Profile", component: ProfilePage },
   { path: "/job/results/:id", name: "JobView", component: JobView },
   { path: "/teams", name: "TeamsView", component: TeamsView },
+  { path: "/login", name: "LoginPage", component: LoginPage },
 ];
 
 const router = createRouter({
